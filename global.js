385938dbb1784a064922ef013879b5aa48e94edb
Cameron Helkuik <cameronhelkuik@gmail.com>
2015-09-13 10:50:09 -0500

    No errors, console recognizes everything, not showing the div

--- global.js
+++ global.js
@@ -1,48 +1,56 @@
-// (document).ready(function(){
-//   //Take your div into one js variable
-//   var newGrit = document.getElementsByClassName("web--new-grit");
-//   //Take the current position (vertical position from top) of your div in the variable
-//   var pos = div.position();
-//   //Now when scroll event trigger do following
-//   (window).scroll(function () {
-//    var windowpos = (window).scrollTop();
-//    //Now if you scroll more than 100 pixels vertically change the class to AfterScroll
-//    // I am taking 100px scroll, you can take whatever you need
-//    if (windowpos >= (pos.top - 10)) {
-//      div.addClass("AfterScroll");
-//    }
-//  });
-// });
-
-window.onload
-function showNewGrit(){
-  var newGrit = document.getElementsByClassName("web--new-grit");
-  
-  var posNewGrit = newGrit.position();
-  
-  (window).scroll(function(){
-    var windowPos = (window).scrollTop();
-    if (windowPos >= (posNewGrit.top - 50)){
-      newGrit.addClass("afterScroll");
-    }
-  });
-};
-
-showNewGrit();
 
 
-var scrollObject = {};
-window.onscroll = getScrollPosition;
+var newGrit = document.getElementsByClassName("web--new-grit");
 
-function getScrollPosition(){
-    scrollObject = {
-       x: window.pageXOffset,
-       y: window.pageYOffset
-    }
-    // If you want to check distance
-    if(scrollObject.y > 200) {
-        // add class
-    } else {
-        // remove class
-    }
+  window.onscroll = showNewGrit;
+
+  function showNewGrit() {
+    if (window.pageYOffset>120) {
+    newGrit.addClass = "afterScroll"
+  }
 }
+
+
+
+// window.onload
+// function showNewGrit(){
+//   var newGrit = document.getElementsByClassName("web--new-grit");
+//
+//   // var posNewGrit = newGrit.position();
+//
+//   var posNewGrit = {};
+//   window.onscroll = getScrollPosition;
+//
+//   function getScrollPosition(){
+//       posNewGrit = {
+//          y: window.pageYOffset
+//       }
+//       // If you want to check distance
+//       if(posNewGrit.y > 200) {
+//         newGrit.addClass("afterScroll");
+//       }
+//   }
+  
+  // (window).scroll(function(){
+ //    var windowPos = (window).scrollTop();
+ //    if (windowPos >= (posNewGrit.top - 50)){
+ //      newGrit.addClass("afterScroll");
+ //    }
+ //  });
+// };
+
+// showNewGrit();
+
+
+// var scrollObject = {};
+// window.onscroll = getScrollPosition;
+//
+// function getScrollPosition(){
+//     scrollObject = {
+//        y: window.pageYOffset
+//     }
+//     // If you want to check distance
+//     if(scrollObject.y > 200) {
+//       newGrit.addClass("afterScroll");
+//     }
+// }
